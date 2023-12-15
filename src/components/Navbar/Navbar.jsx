a487e6b413e52b78ea99a0da7a294012854a3366
mBukator <m.bukator03@gmail.com>
2023-12-15 18:53:51 +0200

    Added authentication modal window

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,23 +4,30 @@ import logoAlt from '../../img/logo-alt.svg';
 import rekl from '../../img/369638665.jpg';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import LoginModal from '../LoginModal/LoginModal';
 
-const Navbar = ({setShow, size}) => {
-  const [clicked, setClicked] = useState(false)
+const Navbar = () => {
+  const [clicked, setClicked] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
+
+  const state = useSelector((state) => state.handleCart);
+
   const handleClick = () => {
     setClicked(!clicked);
   };
   return (
-      <nav className='header'>
+    <>
+      <nav className="header">
         <div className="header__ad">
-          <a href='index.html' className='ad__link'>
-            <img src={rekl} alt='rekl' className='ad__img'/>
+          <a href="index.html" className="ad__link">
+            <img src={rekl} alt="rekl" className="ad__img" />
           </a>
         </div>
 
         <div className="header__main">
           <div className="main__wrapper">
-            <div onClick={handleClick} className='main__menu'>
+            <div onClick={handleClick} className="main__menu">
               <i id="bar" className={clicked ? 'fas fa-times' : 'fas fa-bars'} />
             </div>
             <div className="main__logo-catalog">
@@ -38,7 +45,7 @@ const Navbar = ({setShow, size}) => {
               <button className="catalog__button">
                 <div className="button__content">
                   <i className="fa-solid fa-folder-open button__icon" />
-                  <span className='button__title'>Каталог</span>
+                  <span className="button__title">Каталог</span>
                 </div>
               </button>
             </div>
@@ -48,21 +55,23 @@ const Navbar = ({setShow, size}) => {
               <input type="text" placeholder="Пошук" name="search" className="search__input" />
             </div>
 
-            <div className='user-cart__wrapper'>
-              <div className="user">
+            <div className="user-cart__wrapper">
+              <div className="user" onClick={() => setOpenModal(true)}>
                 <i className="fa-sharp fa-solid fa-user user__icon"></i>
               </div>
               <div className="cart">
-                <Link to='/cart'>
+                <Link to="/cart">
                   <i className="fa-solid fa-cart-shopping cart__icon" />
-                  <span className='cart__count'>{size}</span>
+                  <span className="cart__count">{state.length}</span>
                 </Link>
               </div>
             </div>
           </div>
         </div>
       </nav>
+      <LoginModal open={openModal} onClose={() => setOpenModal(false)} />
+    </>
   );
-}
+};
 
 export default Navbar;
