62bd50911844c969c28d54d878779ae8d8310ebf
mBukator <m.bukator03@gmail.com>
2023-12-28 01:23:14 +0200

    Changed fetching data and some minor changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,37 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect } from 'react';
 
+// CUSTOM HOOKS IMPORTS
+import useFetch from '../../hooks/useFetch';
+
+// AXIOS
+import axios from 'axios';
+import { getAllProducts } from '../../API';
+
+// COMPONENTS
 import ItemCard from '../../components/ItemCard/ItemCard';
 import CategoryItem from '../../components/CategoryItem/CategoryItem';
+import Skeleton from './Skeleton';
 
+// UTILS
 import categories from '../../util/categories';
 
-import './Home.css';
-
+// IMG
 import Information from '../../img/categories-icon/icon-information.png';
 import Message from '../../img/categories-icon/icon-paper-plane.png';
 import Delivery from '../../img/categories-icon/icon-delivery.png';
-import Skeleton from './Skeleton';
 
-const Home = ({ handleAddToCart }) => {
-  const [goods, setGoods] = useState([])
-  const [isLoading, setIsLoading] = useState(true)  //lazy-loading
+import './Home.css';
 
-  useEffect(() => {
-    axios.get("https://65501dfa7d203ab6626d92b6.mockapi.io/goods")
-    .then((response)=>{
-      setGoods(response.data);
-      setIsLoading(false)   //lazy-loading
-    })
-  }, [])
+const Home = () => {
+  const { data: products = [], isLoading } = useFetch(getAllProducts);
 
   return (
     <div className="layout_with_sidebar">
-      <aside className="sidebar desktop-only">
+      <div className="sidebar desktop-only">
         <div className="menu-categories">
           <ul>
             {categories.map((category) => (
-              <CategoryItem key={category.id} category={category}  />
+              <CategoryItem key={category.id} category={category} />
             ))}
           </ul>
         </div>
@@ -60,24 +60,22 @@ const Home = ({ handleAddToCart }) => {
             Точки видачі Rozetka
           </a>
         </div>
-      </aside>
+      </div>
 
-      <main className="main-content">
+      <div className="main-content">
         <div className="goods">
           <div className="more-goods">
             <div className="goods-container">
               <h2>Більше товарів для вибору</h2>
               <section className="more-goods__grid">
-                {
-                  isLoading
-                  ? [...new Array(5)].map(() => <Skeleton/>)
-                  : goods.map((item) => (<ItemCard key={item.id} item={item} handleAddToCart={handleAddToCart} />))
-                }
+                {isLoading
+                  ? [...new Array(25)].map((_, index) => <Skeleton key={index} />)
+                  : products.map((item) => <ItemCard key={item.id} item={item} />)}
               </section>
             </div>
           </div>
         </div>
-      </main>
+      </div>
     </div>
   );
 };
