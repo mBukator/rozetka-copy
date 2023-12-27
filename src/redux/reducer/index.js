55cf6e9a4f701f95059738efbf7836e38ccdde9e
mBukator <m.bukator03@gmail.com>
2023-12-28 01:17:27 +0200

    Added handleChangeCurrency to redux

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from '@reduxjs/toolkit';
 import handleCart from './handleCart'
+import handleChangeCurrency from './handleChangeCurrency';
 
 const rootReducer = combineReducers({
     handleCart,
+    handleChangeCurrency,
 })
 
 export default rootReducer;
