2a4a7eaa07d3ea6965f4fde32f0a16a1263c43ba
mBukator <m.bukator03@gmail.com>
2023-12-28 01:19:58 +0200

    Minor changes in routes and cart handling

--- src/App.js
+++ src/App.js
@@ -1,40 +1,28 @@
 import React, { useState } from 'react';
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-// import Footer from './components/Footer/Footer';
-import Cart from './pages/Cart/Cart';
-import Home from './pages/Home/Home';
 import { Routes, Route } from 'react-router-dom';
 
+import Navbar from './components/Navbar/Navbar';
+
+import Home from './pages/Home/Home';
+import ProductPage from './pages/ProductPage/ProductPage';
+import ProductCharacteristics from './pages/ProductCharacteristics/ProductCharacteristics';
+import ProductComments from './pages/ProductComments/ProductComments';
+
+import './App.css';
+
 const App = () => {
-  const [cart, setCart] = useState([]);
-
-  const handleAddToCart = (item) => {
-    if (cart.indexOf(item) !== -1) return;
-    setCart([...cart, item]);
-  };
-
-  const handleChange = (item, d) => {
-    const ind = cart.indexOf(item);
-    const arr = cart;
-    arr[ind].amount += d;
-
-    if (arr[ind].amount === 0) {
-      arr[ind].amount = 1;
-    }
-    setCart([...arr]);
-  };
-
   return (
     <div className="App">
-      <Navbar size={cart.length} />
+      <Navbar />
       <Routes>
-        <Route path='/' element=<Home handleAddToCart={handleAddToCart} /> />
-        <Route path='/cart' element=<Cart cart={cart} setCart={setCart} handleChange={handleChange}/> />
-        <Route path='*' element=<h1>404</h1> /> {/* !!!!!!!! */}
+        <Route path="/" element=<Home /> />
+        <Route path="/product/:id" element=<ProductPage /> />
+        <Route path={`/product/:id/characteristics`} element=<ProductCharacteristics /> />
+        <Route path={`/product/:id/comments`} element=<ProductComments /> />
+        <Route path="*" element=<h1>404</h1> /> {/* !!!!!!!! */}
       </Routes>
     </div>
   );
-}
+};
 
 export default App;
